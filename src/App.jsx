ef68f24a914d94e5c872ca8d30c06e124cc68c63
Jyotiii Das <163004395+jyotidas4803@users.noreply.github.com>
2024-11-07 08:04:01 +0000

    counter with tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import Counter from "./Counter";
+// import Counter from "./Counter";
+
+// function App() {
+//   return (
+//     <main className="grid place-items-center min-h-screen bg-slate-100">
+//       <div>
+//         <Counter />
+//       </div>
+//     </main>
+//   );
+// }
+
+// export default App;
+
+
+import React from 'react'
+import Counter from './Counter'
 
 function App() {
   return (
-    <main className="grid place-items-center min-h-screen bg-slate-100">
-      <div>
-        <Counter />
-      </div>
+    <main className="container">
+      <Counter start={10} />
+      <Counter />
+      <Counter start="50" step="20" danger />
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,23 +1,56 @@
+// import React, { useState } from "react";
+
+// function Counter() {
+//   const [stateValue, func] = useState(10);
+
+//   const handleClick = () => {
+//     func(100);
+//   };
+
+//   return (
+//     <div className="flex gap-4 text-5xl items-center justify-center border-4 p-3">
+//       <button className="btn" onClick={handleClick}>
+//         -
+//       </button>
+//       <p>{stateValue}</p>
+//       <button className="btn" onClick={handleClick}>
+//         +
+//       </button>
+//     </div>
+//   );
+// }
+
+// export default Counter;
+
+
 import React, { useState } from "react";
 
-function Counter() {
-  const [stateValue, func] = useState(10);
+function Counter({start = 0 , step = 10, danger=false}) {
 
-  const handleClick = () => {
-    func(100);
-  };
+const [counter, updateCounter] = useState(+start);
+
+
+const handleClick = (type) => {
+    if(type==='minus'){
+        updateCounter(counter-10);
+        console.log('clicked-');
+        return
+    }
+    updateCounter(counter+10);
+    console.log('clicked+');
+    
+};
+
+
 
   return (
-    <div className="flex gap-4 text-5xl items-center justify-center border-4 p-3">
-      <button className="btn" onClick={handleClick}>
-        -
-      </button>
-      <p>{stateValue}</p>
-      <button className="btn" onClick={handleClick}>
-        +
-      </button>
+    <div className='flex items-center border-4 p-3 gap-4'>
+
+        <button className={`btn ${danger?'red':''}`} onClick={()=>handleClick('minus')}>-</button>
+        <p className='text-2xl font-bold'>{counter}</p>
+        <button className={`btn ${danger?'red':''}`} onClick={()=>handleClick('plus')}>+</button>
     </div>
-  );
+  )
 }
 
 export default Counter;
